e138efa4c4958f9c27b36e5ead96b1bd60a63d12
Alok <alok@oc4442245506.ibm.com>
2017-10-13 06:33:55 +0530

    modified java program

--- Hello.java
+++ Hello.java
@@ -4,7 +4,7 @@ public class Hello{
 
             for( int i=1;i<=10;i++){
 
-                System.out.println("Hello World India... "+i);
+                System.out.println("I am  Indian... "+i);
                 }
 
            }
